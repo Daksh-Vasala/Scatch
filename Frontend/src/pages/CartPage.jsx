1f822f2c202656d6ce1077a9606599dae73e8c68
Daksh Vasala <vasaladaksh5@gmail.com>
2026-02-06 19:58:37 +0530

    fixing ui

--- Frontend/src/pages/CartPage.jsx
+++ Frontend/src/pages/CartPage.jsx
@@ -64,7 +64,6 @@ function CartPage() {
     <>
       <Navbar />
       <div className="mx-auto py-10 max-w-7xl h-80">
-        <h1 className="text-4xl font-semibold mb-8">Your Cart</h1>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 px-5">
           <div className="lg:col-span-2 space-y-6">
            {cart.items.map((item) => (
